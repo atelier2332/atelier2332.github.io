3fcab41fde308326c02c949f1a8e963091a46254
Thomas Lhoest <tlhoest@gmail.com>
2018-12-03 09:02:44 +0100

    Update links

--- home/js/transitions.js
+++ home/js/transitions.js
@@ -1,36 +1,27 @@
 (function(){
 
+  // mouseover
+
   $('.navlink').on('mouseover', function(e){
     $('body').addClass('blue');
     $('.header').addClass('blue');
     $('.desc').addClass('hidden');
 
     if($(this).attr('id') == 'navlink-left')
-      $('.navlink-title').text('studio');
+      $('.navlink-title').text('portfolio');
     if($(this).attr('id') == 'navlink-right')
-      $('.navlink-title').text('lab');
+      $('.navlink-title').text('github');
 
       $('.navlink-title').addClass('visible');
   });
+
+  // mouseout
 
   $('.navlink').on('mouseout', function(e){
     $('body').removeClass('blue');
     $('.header').removeClass('blue');
     $('.desc').removeClass('hidden');
     $('.navlink-title').removeClass('visible');
-  });
-
-  $('.navlink').on('click', function(e){
-    e.preventDefault();
-    if($(this).attr('id') == 'navlink-left')
-      $('#studio').addClass('is-active');
-    if($(this).attr('id') == 'navlink-right')
-      $('#lab').addClass('is-active');
-  });
-
-  $('.btn-close').on('click', function(e){
-    e.preventDefault();
-    $('.modal').removeClass('is-active');
   });
 
 
